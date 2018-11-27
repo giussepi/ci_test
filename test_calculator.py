d80cc6e69feecd77e26655ba90ce16e70ce5d927
Giussepi Lopez <giussepexy@gmail.com>
2018-11-27 14:58:31 +0000

    calculator - division operation added

--- test_calculator.py
+++ test_calculator.py
@@ -16,3 +16,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 2 == calculator.division(8, 4)
